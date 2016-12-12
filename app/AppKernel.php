8c57138705443384b1f05dd1e02897cd38393b9c
vtiertant <vinz.tiertant@gmail.com>
2016-12-12 17:08:35 +0100

    #3 : configure JMS translation bundle routing

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new FOS\UserBundle\FOSUserBundle(),
             new JMS\I18nRoutingBundle\JMSI18nRoutingBundle(),
+            new JMS\TranslationBundle\JMSTranslationBundle(),
             new AppBundle\AppBundle(),
             new AdminBundle\AdminBundle(),
 
